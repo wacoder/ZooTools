f8df685b5f8186b7db5563921969fc54655e110e
wacoder <wangjing5802@126.com>
2016-07-17 16:28:24 -0400

    mapper reducer

---
+++ saleSum/reducer.py
@@ -0,0 +1,20 @@
+import sys
+
+oldKey = None
+saleSum = 0
+
+for line in sys.stdin:
+    data_mapped = line.strip().split('\t')
+    if len(data_mapped) != 2:
+        continue
+    thiskey, thisValue = data_mapped
+
+    if oldKey and oldKey != thiskey:
+        print oldKey,'\t',saleSum
+        oldKey = thiskey
+        saleSum = 0
+    oldKey = thiskey
+    saleSum = saleSum + float(thisValue)
+
+if oldKey != None:
+    print oldKey,'\t',saleSum
