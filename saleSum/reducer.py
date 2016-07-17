8331d0b13753f1f9de1e5d12854940c15e56e5c4
wacoder <wangjing5802@126.com>
2016-07-17 17:54:41 -0400

    get summary

--- saleSum/reducer.py
+++ saleSum/reducer.py
@@ -4,6 +4,8 @@ import sys
 oldKey = None
 saleSum = 0
 saleMax = 0
+totalNum = 0
+totalValue = 0
 
 for line in sys.stdin:
     data_mapped = line.strip().split('\t')
@@ -14,11 +16,15 @@ for line in sys.stdin:
     if oldKey and oldKey != thiskey:
         print oldKey,'\t',saleSum,'\t',saleMax
         oldKey = thiskey
+        totalValue += saleSum
         saleSum = 0
         saleMax = 0
     oldKey = thiskey
     saleSum = saleSum + float(thisValue)
-    saleMax = max(saleMax,thisValue)
+    saleMax = max(saleMax,float(thisValue))
+    totalNum +=1
 
 if oldKey != None:
     print oldKey,'\t',saleSum,'\t',saleMax
+    totalValue += saleSum
+    print "The summary is:",totalNum,'\t',totalValue
